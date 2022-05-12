ce543d2986fe0ac7ab3012b42cb9e835da825daa
Kongratbayev Nurzhan <kongratbaev.77.7@gmail.com>
2022-05-12 12:03:47 +0600

    Start Porject

---
+++ src/main/java/kz/nurzhan/technodom/TechnodomApplication.java
@@ -0,0 +1,13 @@
+package kz.nurzhan.technodom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnodomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnodomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/nurzhan/technodom/TechnodomApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.nurzhan.technodom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnodomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
